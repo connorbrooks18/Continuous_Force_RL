064937a74d34e665277130575547bcbb0eeed0f0
Connor Brooks <connorbrooks18@icloud.com>
2026-08-20 15:38:24 -0700

    updating calibration constants

--- real_robot_exps/apple_pullto_static.py
+++ real_robot_exps/apple_pullto_static.py
@@ -1610,7 +1610,7 @@ def main():
 
     # arbitrarily chosen 'home'
     home_rot = np.array([[-1, 0, 0.0], [0.0, 0.0, 1.0], [0, 1, 0]])
-    home_pos = np.array([0.01, 0.59, 0.42])
+    home_pos = np.array([0.01, 0.59, 0.40])
     home_pose_4x4 = make_ee_target_pose_from_matrix(home_pos, home_rot)
 
     apple_rot = np.array([
